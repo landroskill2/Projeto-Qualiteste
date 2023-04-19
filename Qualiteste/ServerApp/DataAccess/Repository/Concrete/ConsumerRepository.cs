4fcf0f224ebb9156fa9d245c698d57c6bebc6507
jamendes <jamendes99@hotmail.com>
2023-04-19 17:58:21 +0000

    Refactored GetConsumersFiltered to use predicate

--- Qualiteste/ServerApp/DataAccess/Repository/Concrete/ConsumerRepository.cs
+++ Qualiteste/ServerApp/DataAccess/Repository/Concrete/ConsumerRepository.cs
@@ -2,6 +2,10 @@
 using Qualiteste.ServerApp.DataAccess.Concrete;
 using Qualiteste.ServerApp.Dtos;
 using Qualiteste.ServerApp.Models;
+using System.Drawing;
+using System;
+using System.Linq.Expressions;
+using System.Xml;
 
 namespace Qualiteste.ServerApp.DataAccess.Repository.Concrete
 {
@@ -22,22 +26,24 @@ namespace Qualiteste.ServerApp.DataAccess.Repository.Concrete
             return PostgresContext.Consumers.Single(c => c.Id == id);
         }
 
-        public IEnumerable<Consumer> GetConsumersFiltered(string sex, int iage, string name)
+        public IEnumerable<Consumer> GetConsumersFiltered(string sex, int iage, string name) {
+            Expression<Func<Consumer, bool>> p = c => Filter(c, sex, iage, name);
+            return GetConsumersFiltered(p);
+        }
+
+        public bool Filter(Consumer c, string sex, int iage, string name)
+        {
+            bool sexCondition = sex == "*" ? true : c.Sex == sex;
+            bool nameCondition = name == "*" ? true : c.Fullname.Contains(name);
+            bool ageCondition = (DateTime.Today.Year - c.Dateofbirth.Value.Year) >= iage;
+
+            return sexCondition && nameCondition && ageCondition;
+        }
+        public IEnumerable<Consumer> GetConsumersFiltered(Expression<Func<Consumer, bool>> p)
         {
             IEnumerable<Consumer> consumers;
-            if (sex.Equals("*"))
-            {
-                consumers = PostgresContext.Consumers.Where(c => (DateTime.Today.Year - c.Dateofbirth.Value.Year) >= iage).OrderBy(c => c.Fullname);
-            }
-            else 
-            {
-                consumers = PostgresContext.Consumers.Where(c =>
-                    c.Sex == sex &&
-                    (DateTime.Today.Year - c.Dateofbirth.Value.Year) >= iage
-                ).OrderBy(c => c.Fullname);
-            }
-            if (name.Equals("*")) return consumers;
-            return consumers.Where(c => c.Fullname.Contains(name));
+            consumers = PostgresContext.Consumers.Where(p).OrderBy(c => c.Fullname);
+            return consumers;
         }
 
         public PostgresContext PostgresContext
