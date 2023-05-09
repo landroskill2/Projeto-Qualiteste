2d67a03a80f93cbc1ce7ffe8e0a5c7ef87414c66
jamendes <jamendes99@hotmail.com>
2023-05-09 17:09:28 +0000

    Changed ConsumersRepository.GetConsumersFiltered Added GetConsumersInTest to TestRepository

--- Qualiteste/ServerApp/DataAccess/Repository/ITestRepository.cs
+++ Qualiteste/ServerApp/DataAccess/Repository/ITestRepository.cs
@@ -4,8 +4,10 @@ namespace Qualiteste.ServerApp.DataAccess.Repository
 {
     public interface ITestRepository : IRepository<Test>
     {
+        IEnumerable<Consumer> GetConsumersInTest(string id);
         Test? GetTestById(string id);
         IEnumerable<Test> ListTestsByDate();
         IEnumerable<Test> ListTestsWithFilters(string type);
+
     }
 }
